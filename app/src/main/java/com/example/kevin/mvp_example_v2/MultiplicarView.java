8b6385aec6ab8002d44e226d549b8a5163897673
kevinrrdev <kevinreyesrojas3@gmail.com>
2018-06-07 15:39:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kevin/mvp_example_v2/MultiplicarView.java
@@ -0,0 +1,13 @@
+package com.example.kevin.mvp_example_v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiplicarView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiplicar_view);
+    }
+}
